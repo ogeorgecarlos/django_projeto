7242be0de0b27df5d9cabae959b794cde9d9e552
George carlos Gomes <georgecarlos@live.com>
2022-12-31 13:15:30 +0000

    removed 'cotatos e sobre'

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from recipes.views import home, contatos, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('contatos/', contatos),
-    path('sobre/', sobre),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,10 +6,3 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'recipes/home.html')
 
-
-def contatos(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return HttpResponse('sobre')
